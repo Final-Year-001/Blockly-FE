21103dc6e3ab9b73d61bcd61f371a4c6ed9e31dc
Dehemi Weerakkody <dehemiweerakkodi2@gmail.com>
2023-11-04 01:03:51 +0530

    UI updates and deps

--- src/components/sandboxTopBar.tsx
+++ src/components/sandboxTopBar.tsx
@@ -1,14 +1,34 @@
-import { Button } from "@material-tailwind/react";
+import { Button, Select, Option, Typography } from "@material-tailwind/react";
 import { PlayIcon } from "@heroicons/react/24/solid";
 import LoadingIcon from "../components/loadingicon";
 import { useState } from "react";
+import { useQuery } from "react-query";
 
 function SandboxTopBar() {
   const [loading, setLoading] = useState<boolean>(false);
+  const sandboxQuery = useQuery(["sandboxes"]);
 
   return (
     <div className="flex w-full justify-between items-center p-4">
-      <div></div>
+      <div>
+        {sandboxQuery.isFetching ? (
+          <div className="flex flex-row gap-3 items-center p-2 w-72">
+            <LoadingIcon className="animate-spin h-4 w-" />
+            <Typography variant="small">Loading available sandboxes</Typography>
+          </div>
+        ) : null}
+        {sandboxQuery.isSuccess ? (
+          <div className="w-72">
+            <Select label="Select Sandbox">
+              <Option>Material Tailwind HTML</Option>
+              <Option>Material Tailwind React</Option>
+              <Option>Material Tailwind Vue</Option>
+              <Option>Material Tailwind Angular</Option>
+              <Option>Material Tailwind Svelte</Option>
+            </Select>
+          </div>
+        ) : null}
+      </div>
       <div>
         <Button
           variant="filled"
