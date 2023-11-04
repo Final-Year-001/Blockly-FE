410ef8d90ec3b814f40e1cbc6ae806246045f3f2
Semora <senalweerasekara@gmail.com>
2023-11-04 15:04:10 +0530

    frontend facelift

--- src/pages/fronted/index.tsx
+++ src/pages/fronted/index.tsx
@@ -2,20 +2,80 @@ import { useRef, useState } from 'react';
 import FrontendWorkspace from '../../workspaces/frontend/frontendWorkspace';
 import { ClipboardIcon  } from '@heroicons/react/24/solid';
 import { CheckCircleIcon } from '@heroicons/react/24/solid';
+import {
+  Tabs,
+  TabsHeader,
+  Tab,
+  TabsBody,
+  TabPanel,
+  Avatar,
+} from "@material-tailwind/react";
+import { useRecoilState } from "recoil";
+import { codeAtom } from "../../state/code";
+
+function organizeImports(code: string) {
+  // Split the code into lines
+  const lines = code.split("\n");
+
+  // Extract import statements and other code
+  const importStatements = [];
+  const otherCode = [];
+
+  for (const line of lines) {
+    if (line.trim().startsWith("import ")) {
+      importStatements.push(line);
+    } else {
+      otherCode.push(line);
+    }
+  }
+
+  // Combine import statements and other code
+  const organizedCode =
+    importStatements.join("\n") + "\n" + otherCode.join("\n");
+
+  return organizedCode;
+}
 
 function FrontendPage() {
   const [frontendCode, setFrontendCode] = useState('');
   const [tabView, setTabView] = useState('code'); // "code" or "iframe"
   const iframeRef = useRef<HTMLIFrameElement>(null);
   const [codeCopied, setCodeCopied] = useState(false);
+  let [code, setCode] = useRecoilState(codeAtom);
 
   const injectCode = (code: string) => {
+    setCode(organizeImports(code))
     setFrontendCode(code);
     if (iframeRef.current) {
       const iframe = iframeRef.current;
       iframe.srcdoc = code;
     }
   };
+
+  const tabs = [
+    {
+      label: "Code",
+      value: "html",
+      desc: (
+        <div className="whitespace-pre-line w-full h-full p-2 text-white">
+          <code>{code}</code>
+        </div>
+      ),
+    },
+    {
+      label: "IFrame",
+      value: "iframe",
+      desc:   <iframe className='bg-white w-full h-full' ref={iframeRef} name="iframe1" />
+    },
+    {
+      label: "Console",
+      value: "react",
+      desc: `Because it's about motivating the doers. Because I'm here
+      to follow my dreams and inspire other people to follow their dreams, too.`,
+    },
+  ];
+  
+
 
   const copyCodeToClipboard = (code: string) => {
     const textField = document.createElement('textarea');
@@ -37,25 +97,25 @@ function FrontendPage() {
   };
 
   return (
-    <>
-      <h1 className="text-3xl font-bold mt-6 mb-4 text-indigo-400">
+    <div className="flex flex-col h-full w-full ">
+      {/* <h1 className="text-3xl font-bold mt-6 mb-4 text-indigo-400">
         Frontend workspace
-      </h1>
+      </h1> */}
 
       <div
-        style={{
-          display: 'flex',
-          flexDirection: 'row',
-          height: '100%',
-          whiteSpace: 'pre-line',
-        }}
+        className="flex flex-row flex-grow px-6 pb-4"
+        style={{ height: "calc(100% - 400px)" }}
       >
-        <div style={{ flex: 0.7, padding: '0 10px' }}>
+
+        <div className="flex-[0.7]">
           <FrontendWorkspace onCodeChange={injectCode} />
         </div>
+        {/* <div style={{ flex: 0.7, padding: '0 10px' }}>
+          <FrontendWorkspace onCodeChange={injectCode} />
+        </div> */}
 
-        <div style={{ flex: 0.3, backgroundColor: '#EDEDED', padding: 20 }}>
-          <div style={{ display: 'flex', justifyContent: 'center' }}>
+        {/* <div style={{ flex: 0.3, backgroundColor: '#EDEDED', padding: 20 }}> */}
+          {/* <div style={{ display: 'flex', justifyContent: 'center' }}>
             <button
               onClick={() => setTabView('code')}
               style={{
@@ -77,9 +137,57 @@ function FrontendPage() {
             >
               IFrame
             </button>
-          </div>
-          <br />
-          {tabView === 'code' && (
+          </div> */}
+
+          
+          <div className="flex-[0.3] pl-6 h-full ">
+          <Tabs value="html" className="h-full pb-10">
+            <TabsHeader>
+              {tabs.map(({ label, value }) => (
+                <Tab key={value} value={value}>
+                  {label}
+                </Tab>
+              ))}
+            </TabsHeader>
+            <TabsBody className="h-full pb-10  bg-black border rounded-xl hover:overflow-auto">
+              {tabs.map(({ value, desc }) => (
+                <TabPanel key={value} value={value}>
+                  {desc}
+                  {value == 'html' && 
+                  <button
+                style={{ position: 'absolute', top: '10px', right: '10px', background: 'none', border: 'none', cursor: 'pointer', display: 'flex', alignItems: 'center', padding: '4px',}}
+                onClick={() => { copyCodeToClipboard(frontendCode);}}  
+                title="Copy Code"
+              >
+                <ClipboardIcon  className="w-5 h-5 text-indigo-600" />
+              </button>
+              }
+              {codeCopied && (
+                <div
+                  style={{
+                    position: 'absolute',
+                    top: '20px',
+                    right: '20px',
+                    backgroundColor: 'rgba(255, 255, 255, 0.8)', // Transparent background
+                    padding: '4px 8px',
+                    borderRadius: '4px',
+                    boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',
+                    zIndex: 1,
+                    fontSize: '12px', 
+                    color: 'grey', 
+                  }}
+                >
+                  <span>Code Copied!</span>
+                </div>
+              )}
+                </TabPanel>
+              ))}
+            </TabsBody>
+          </Tabs>
+        {/* </div> */}
+
+
+          {/* {tabView === 'code' && (
             <div
               style={{
                 minHeight: '450px',
@@ -112,25 +220,26 @@ function FrontendPage() {
               </button>
               {codeCopied && (
                 <div
-                style={{
-                  position: 'absolute',
-                  top: '20px',
-                  right: '20px',
-                  backgroundColor: 'rgba(255, 255, 255, 0.8)', // Transparent background
-                  padding: '4px 8px',
-                  borderRadius: '4px',
-                  boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',
-                  zIndex: 1,
-                  fontSize: '12px', 
-                  color: 'grey', 
-                }}
+                  style={{
+                    position: 'absolute',
+                    top: '20px',
+                    right: '20px',
+                    backgroundColor: 'rgba(255, 255, 255, 0.8)', // Transparent background
+                    padding: '4px 8px',
+                    borderRadius: '4px',
+                    boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',
+                    zIndex: 1,
+                    fontSize: '12px', 
+                    color: 'grey', 
+                  }}
                 >
                   <span>Code Copied!</span>
                 </div>
               )}
             </div>
-          )}
-          {tabView === 'iframe' && (
+          )} */}
+
+          {/* {tabView === 'iframe' && (
             <div
               style={{
                 minHeight: '450px',
@@ -141,10 +250,10 @@ function FrontendPage() {
             >
               <iframe ref={iframeRef} name="iframe1" />
             </div>
-          )}
+          )} */}
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
