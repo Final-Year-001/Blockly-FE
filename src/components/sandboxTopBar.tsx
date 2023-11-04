2028b11ef6e64be844016c9501254e78fb2583ac
Dehemi Weerakkody <dehemiweerakkodi2@gmail.com>
2023-11-04 11:49:01 +0530

    Added sandbox intergration

--- src/components/sandboxTopBar.tsx
+++ src/components/sandboxTopBar.tsx
@@ -1,24 +1,60 @@
-import { Button, Select, Option, Typography, Avatar } from "@material-tailwind/react";
+import {
+  Button,
+  Select,
+  Option,
+  Typography,
+  Avatar,
+} from "@material-tailwind/react";
 import { PlayIcon, ExclamationCircleIcon } from "@heroicons/react/24/solid";
 import LoadingIcon from "../components/loadingicon";
 import { useState } from "react";
-import { useQuery } from "react-query";
+import { useQuery, useMutation } from "react-query";
 import { httpClient } from "../helpers/axios";
+import { useRecoilState, useRecoilValue } from "recoil";
+import { sandboxAtom } from "../state/stadbox";
+import { codeAtom } from "../state/code";
 
 function SandboxTopBar() {
-  const [loading, setLoading] = useState<boolean>(false);
-  const sandboxQuery = useQuery(["sandboxes"], () => httpClient.get("sandbox/all"));
+  const [sandbox, setSandbox] = useRecoilState(sandboxAtom);
+  const code = useRecoilValue(codeAtom);
+
+  const sandboxQuery = useQuery(["sandboxes"], () =>
+    httpClient.get("sandbox/all")
+  );
+  
   const containers = sandboxQuery.data?.data?.containers || [];
+
+  const codeMutation = useMutation({
+    mutationFn: async () =>{
+      let c = `
+          const express = require('express')
+          const app = express();
+          ${code}
+          app.get("/health", (req, res) => {
+              res.json({ hello: "Hello world!" });
+          })
+          
+          app.listen("8999", () => {
+              console.log("listen on 8999")
+          })
+      `
+      httpClient.post("sandbox/" + sandbox.name, c, {
+        headers: { "Content-Type": "text/plain" },
+      })
+    }
+  });
 
   return (
     <div className="flex w-full justify-between items-center p-4">
       <div className="flex flex-row gap-3 px-2">
-      <Button
+        <Button
           variant="filled"
           className="flex flex-row gap-3 justify-center items-center text-white"
-          onClick={() => setLoading(!loading)}
+          onClick={() => {
+            codeMutation.mutate();
+          }}
         >
-          {loading ? (
+          {codeMutation.isLoading ? (
             <LoadingIcon className="animate-spin h-4 w-" />
           ) : (
             <PlayIcon className="h-4 w-4" />
@@ -39,18 +75,25 @@ function SandboxTopBar() {
         ) : null}
         {sandboxQuery.isSuccess && !sandboxQuery.isFetching ? (
           <div className="w-[30em]">
-            <Select label="Select Sandbox" defaultValue={containers[0]?.name}>
-              {containers.map((c: {name:string}) => <Option>{c.name}</Option>)}
+            <Select
+              label="Select Sandbox"
+              value={sandbox.name}
+              onChange={(s) =>
+                setSandbox((prev) => {
+                  return { ...prev, name: s };
+                })
+              }
+            >
+              {containers.map((c: { name: string }) => (
+                <Option value={c.name}>{c.name}</Option>
+              ))}
             </Select>
           </div>
         ) : null}
       </div>
+      <div></div>
       <div>
-        
-      </div>
-      <div>
-      <Avatar src="/img/cat default avatar.png" alt="avatar" size="md" />
-
+        <Avatar src="/img/cat default avatar.png" alt="avatar" size="md" />
       </div>
     </div>
   );
