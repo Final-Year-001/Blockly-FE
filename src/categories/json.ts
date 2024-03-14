3cd061992cbc458943fdf410b0547ab5464565a4
nisal <it20081034@my.sliit.lk>
2024-03-14 19:54:15 +0530

    add json category

---
+++ src/categories/json.ts
@@ -0,0 +1,17 @@
+import "../blocks/node/jsonBlocks";
+
+export const jsonCategory = {
+  kind: "category",
+  name: "Json hanldlers",
+  colour: "#000000",
+  contents: [
+    {
+      kind: "block",
+      type: "extract_value",
+    },
+    {
+      kind: "block",
+      type: "key_chain",
+    },
+  ],
+};
