213cad233f17577bf926b1ad25d5f50aed3a111e
Semora <senalweerasekara@gmail.com>
2024-05-17 22:38:12 +0530

    added more features to the doc page layout explanation part

--- src/pages/documents/gettingStarted/LayoutGuide.tsx
+++ src/pages/documents/gettingStarted/LayoutGuide.tsx
@@ -3,6 +3,7 @@ import top from "../../../assets/LayoutTour/1.png";
 import sb from "../../../assets/LayoutTour/2.png";
 import wa from "../../../assets/LayoutTour/3.png";
 import co from "../../../assets/LayoutTour/4.png";
+import { blueButton } from "../../../assets/buttons/Buttons";
 
 const style = "transition  duration-100 cursor-pointer relative";
 const highlight = "border-2 border-red-500";
@@ -18,7 +19,7 @@ const outputContent =
 
 function LayoutGuide() {
   const [hoveredImage, setHoveredImage] = useState(null);
-  const [selected, setSelected] = useState("");
+  const [selected, setSelected] = useState(0);
 
   const handleMouseEnter = (image: any) => {
     setHoveredImage(image);
@@ -36,10 +37,14 @@ function LayoutGuide() {
             className={style}
             onMouseEnter={() => handleMouseEnter("top")}
             onMouseLeave={handleMouseLeave}
-            onClick={() => setSelected("topbar")}
+            onClick={() => setSelected(1)}
           >
             <div
-              className={`absolute top-0 left-0 w-full h-full flex items-center justify-center bg-black ${selected == "topbar" && highlight} ${selected == "topbar" && "bg-opacity-0"} bg-opacity-40 rounded font-bold text-lg z-10 ${
+              className={`absolute top-0 left-0 w-full h-full flex items-center justify-center bg-black ${
+                selected == 1 && highlight
+              } ${
+                selected == 1 && "bg-opacity-0"
+              } bg-opacity-40 rounded font-bold text-lg z-10 ${
                 hoveredImage === "top" && "hidden"
               }`}
             >
@@ -52,10 +57,14 @@ function LayoutGuide() {
               className={style}
               onMouseEnter={() => handleMouseEnter("sb")}
               onMouseLeave={handleMouseLeave}
-              onClick={() => setSelected("sidebar")}
+              onClick={() => setSelected(2)}
             >
               <div
-                className={`absolute top-0 left-0 w-full h-full flex items-center justify-center bg-black  ${selected == "sidebar" && highlight} ${selected == "sidebar" && "bg-opacity-0"} bg-opacity-40 rounded font-bold text-lg z-10 ${
+                className={`absolute top-0 left-0 w-full h-full flex items-center justify-center bg-black  ${
+                  selected == 2 && highlight
+                } ${
+                  selected == 2 && "bg-opacity-0"
+                } bg-opacity-40 rounded font-bold text-lg z-10 ${
                   hoveredImage === "sb" && "hidden"
                 }`}
               >
@@ -67,10 +76,14 @@ function LayoutGuide() {
               className={style}
               onMouseEnter={() => handleMouseEnter("wa")}
               onMouseLeave={handleMouseLeave}
-              onClick={() => setSelected("workarea")}
+              onClick={() => setSelected(3)}
             >
               <div
-                className={`absolute top-0 left-0 w-full h-full flex items-center justify-center bg-black  ${selected == "workarea" && highlight} ${selected == "workarea" && "bg-opacity-0"} bg-opacity-40 rounded font-bold text-lg z-10 ${
+                className={`absolute top-0 left-0 w-full h-full flex items-center justify-center bg-black  ${
+                  selected == 3 && highlight
+                } ${
+                  selected == 3 && "bg-opacity-0"
+                } bg-opacity-40 rounded font-bold text-lg z-10 ${
                   hoveredImage === "wa" && "hidden"
                 }`}
               >
@@ -82,10 +95,14 @@ function LayoutGuide() {
               className={style}
               onMouseEnter={() => handleMouseEnter("co")}
               onMouseLeave={handleMouseLeave}
-              onClick={() => setSelected("codeoutput")}
+              onClick={() => setSelected(4)}
             >
               <div
-                className={`absolute top-0 left-0 w-full h-full flex items-center justify-center bg-black  ${selected == "codeoutput" && highlight} ${selected == "codeoutput" && "bg-opacity-0"} bg-opacity-40 rounded font-bold text-lg z-10 ${
+                className={`absolute top-0 left-0 w-full h-full flex items-center justify-center bg-black  ${
+                  selected == 4 && highlight
+                } ${
+                  selected == 4 && "bg-opacity-0"
+                } bg-opacity-40 rounded font-bold text-lg z-10 ${
                   hoveredImage === "co" && "hidden"
                 }`}
               >
@@ -99,29 +116,73 @@ function LayoutGuide() {
 
       <div className="w-3/12">
         <div className="bg-gray-200 p-6 shadow-lg mx-4 text-justify rounded-lg">
-          {!selected && "Click on something to get more information"}
-          {selected == "topbar" && (
+          {selected == 0 && (
+            <div>
+              <div className="text-2xl">Start Tour</div>
+              <div>
+                This will quickly show what are the main components of the
+                frontend builder. You can get a quick idea where things are with
+                this. Click start to begin
+              </div>
+              <div
+                onClick={() => setSelected(1)}
+                className={`${blueButton} mt-10 flex shadow`}
+              >
+                Start
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="m8.25 4.5 7.5 7.5-7.5 7.5"
+                  />
+                </svg>
+              </div>
+            </div>
+          )}
+          {selected == 1 && (
             <div>
               <div className="text-2xl">Top Bar</div>
               <div>{topBarContent}</div>
+              <div className="flex justify-end mt-4 gap-4">
+                <div onClick={()=>setSelected(selected + 1)}className={`${blueButton} px-6`}>Next</div>
+              </div>
             </div>
           )}
-          {selected == "sidebar" && (
+          {selected == 2 && (
             <div>
               <div className="text-2xl">Side Bar</div>
               <div>{sideBarContent}</div>
+              <div className="flex justify-end mt-4 gap-4">
+                <div onClick={()=>setSelected(selected - 1)} className={`${blueButton} px-6`}>Back</div>
+                <div onClick={()=>setSelected(selected + 1)}className={`${blueButton} px-6`}>Next</div>
+              </div>
             </div>
           )}
-          {selected == "workarea" && (
+          {selected == 3 && (
             <div>
               <div className="text-2xl">Work Area</div>
               <div>{workAreaContent}</div>
+              <div className="flex justify-end mt-4 gap-4">
+                <div onClick={()=>setSelected(selected - 1)} className={`${blueButton} px-6`}>Back</div>
+                <div onClick={()=>setSelected(selected + 1)}className={`${blueButton} px-6`}>Next</div>
+              </div>
             </div>
           )}
-          {selected == "codeoutput" && (
+          {selected == 4 && (
             <div>
               <div className="text-2xl">Output Area</div>
               <div>{outputContent}</div>
+              <div className="flex justify-end mt-4 gap-4">
+                <div onClick={()=>setSelected(selected - 1)} className={`${blueButton} px-6`}>Back</div>
+                <div onClick={()=>setSelected(0)}className={`${blueButton} px-6`}>Finish</div>
+              </div>
             </div>
           )}
         </div>
