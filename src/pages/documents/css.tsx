9ed5251729a4623949b1e48c1fdc4572015edcde
madda01 <it20232368@my.sliit.lk>
2024-05-18 09:20:18 +0530

    css side bar color changes are fixed

--- src/pages/documents/css.tsx
+++ src/pages/documents/css.tsx
@@ -7,9 +7,9 @@ import Confetti from "react-dom-confetti";
 
 const cardColor = "bg-white/0";
 const topBarColor = "bg-wbcMain";
-const sideBarColor = "bg-gray-200";
-const sideBarHover = "bg-wbcMain";
-const sideBarActive = "hover:bg-wbcMain";
+const sideBarColor = "bg-gray-200"; 
+const sideBarHover = "bg-blue-500";
+const sideBarActive = "hover:bg-blue-600";
 const blueButton =
   "bg-blue-400 hover:bg-blue-500 cursor-pointer active:bg-blue-700 mb-10 p-2 rounded-lg border-black border-2";
 
